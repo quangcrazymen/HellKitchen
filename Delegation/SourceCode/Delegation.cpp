5577b7f47ce419bd4e2d25b0d696b9a0f43d7829
quang nguyen <quangcrazymen@gmail.com>
2024-05-16 08:39:22 +0700

    + Draw stuff in global array

--- Delegation/SourceCode/Delegation.cpp
+++ Delegation/SourceCode/Delegation.cpp
@@ -1,1 +1,3 @@
 #include "Delegation.h"
+
+//https://blog.molecular-matters.com/2011/09/19/generic-type-safe-delegates-and-events-in-c/
--- Delegation/SourceCode/Main.cpp
+++ Delegation/SourceCode/Main.cpp
@@ -4,8 +4,13 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include "World.h"
+#include <vector>
 
-void renderingThread(sf::RenderWindow* window) {
+std::vector<sf::CircleShape> ListOfShape;
+World* world = nullptr;
+
+void renderingThread(sf::RenderWindow* window/*, const World* world*/) {
     window->setActive(true);
     sf::Clock deltaClock;
 
@@ -18,13 +23,16 @@ void renderingThread(sf::RenderWindow* window) {
 
         // set the shape color to green
         shape.setFillColor(sf::Color(100, 250, 50));
+        for (auto& someShape : ListOfShape) {
+            window->draw(someShape);
+        }
         
         //shape.
         window->draw(shape);
 
         window->display();
     }
-    //window->setActive(false);
+    window->setActive(false);
 }
 
 int main()
@@ -32,28 +40,35 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
     std::cout << "Hello World!\n";
 
+    // Init data
+    sf::CircleShape a(20.0f);
+    a.setPosition(100.0f, 200.0f);
+    a.setFillColor(sf::Color(100, 250, 50));
+    ListOfShape.push_back(a);
     window.setActive(false);
 
     sf::Thread thread(&renderingThread, &window);
     thread.launch();
 
-    sf::Clock deltaClock;
+    //sf::Clock deltaClock;
     while (window.isOpen())
     {
-
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed) {
-                window.close();
                 thread.terminate();
+                window.close();
             }
+            //if ()
+            // Click to spawn object on screen
 
         }
     }
-    //thread.terminate();
+    // Clean up 
+    delete world;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
